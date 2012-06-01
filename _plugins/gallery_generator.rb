d1fb3e1d0c03f1d87a5fa90fa1ed450a3272d11b
Geoff Greer <geoff@greer.fm>
2012-06-01 00:53:00 -0700

    Start of photo plugin

---
+++ _plugins/gallery_generator.rb
@@ -0,0 +1,44 @@
+module Jekyll
+  class GalleryPage < Page
+    def initialize(site, base, dir, gallery_name)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'gallery.html'
+
+      puts "processing..."
+      self.process(@name)
+      puts "done"
+      self.read_yaml(File.join(base, '_layouts'), 'gallery_index.html')
+      puts "read yaml"
+      self.data['gallery'] = gallery_name
+      gallery_title_prefix = site.config['gallery_title_prefix'] || 'Photos: '
+      self.data['title'] = "#{gallery_title_prefix}#{gallery_name}"
+      self.data['images'] = Dir.entries(dir)
+      puts "data", self.data
+    end
+  end
+
+  class GalleryGenerator < Generator
+    safe true
+
+    def generate(site)
+      unless site.layouts.key? 'gallery_index'
+        return
+      end
+      dir = site.config['gallery_dir'] || 'photos'
+      Dir.foreach(dir) do |gallery_dir|
+        gallery_path = File.join(dir, gallery_dir)
+        if File.directory?(gallery_path) and gallery_dir.chars.first != "."
+          puts "generating gallery for", gallery_path
+          gallery = GalleryPage.new(site, site.source, gallery_path, gallery_dir)
+          puts "rendering..."
+          gallery.render(site.layouts, site.site_payload)
+          puts "writing..."
+          gallery.write(site.dest)
+          site.pages << gallery
+        end
+      end
+    end
+  end
+end
