73e362c7c70bdbd71629a7df8e82c023338aab55
Geoff Greer <geoff@greer.fm>
2013-03-08 06:05:36 -0800

    Ignore base less

--- less.sh
+++ less.sh
@@ -2,6 +2,9 @@
 
 for less_file in styles/*.less
 do
+  if [[ $less_file == "styles/main-base.less" ]]; then
+    continue
+  fi
   css_file=`echo $less_file | sed -E "s/.less//"`
   echo "lessc $less_file $css_file.css"
   lessc $less_file $css_file.css
