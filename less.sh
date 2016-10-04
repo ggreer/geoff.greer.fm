6f780780b64132bcc21dbd8f0d9293499ee5a055
Geoff Greer <geoff@greer.fm>
2016-10-04 14:27:32 -0700

    Shellcheck less.sh

--- less.sh
+++ less.sh
@@ -5,7 +5,7 @@ do
   if [[ $less_file == "styles/main-base.less" ]]; then
     continue
   fi
-  css_file=`echo $less_file | sed -E "s/.less//"`
+  css_file=$(echo "$less_file" | sed -E "s/.less//")
   echo "lessc $less_file $css_file.css"
-  lessc $less_file $css_file.css
+  lessc "$less_file" "$css_file.css"
 done
