9530087e235ed07df358e91d18ed5bf9e2ed404d
Geoff Greer <geoff@greer.fm>
2017-09-20 21:22:15 -0700

    Minify css. Don't generate css for colors.less

--- less.sh
+++ less.sh
@@ -5,7 +5,10 @@ do
   if [[ $less_file == "styles/main-base.less" ]]; then
     continue
   fi
+  if [[ $less_file == "styles/colors.less" ]]; then
+    continue
+  fi
   css_file=$(echo "$less_file" | sed -E "s/.less//")
   echo "lessc $less_file $css_file.css"
-  lessc "$less_file" "$css_file.css"
+  lessc -x "$less_file" "$css_file.css"
 done
