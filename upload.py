691802b7ee643d45c5949e7a4a7e53b8454a5f2b
Geoff Greer <geoff@greer.fm>
2017-09-20 21:24:33 -0700

    Use gevent to parallelize uploads and deletes.

--- upload.py
+++ upload.py
@@ -5,6 +5,10 @@ import hashlib
 import os
 import sys
 
+from gevent import monkey
+from gevent.pool import Pool
+monkey.patch_all()
+
 from libcloud.storage.types import Provider, ContainerDoesNotExistError
 from libcloud.storage.providers import get_driver
 
@@ -45,17 +49,53 @@ def md5(path):
     return md5sum.hexdigest()
 
 
+def get_max_age(path):
+    DAY = 60 * 60 * 24
+    WEEK = 7 * DAY
+    YEAR = 365 * DAY
+    path = path.lower()
+    if path.endswith(('.jpg', '.jpeg', '.png', '.gif', '.mp3', '.mp4', '.avi', '.mpg', '.mpeg', '.gz', '.gz.asc')):
+        return YEAR
+    if path.endswith(('.css', '.js', '.asc', '.pdf', '.m4r', '.ico')):
+        return WEEK
+    if path.endswith(('.htm', '.html', '.txt', '.md', '.xml')):
+        return DAY
+    return DAY
+
+
 def upload_file(container, file_path, cf_path):
-    print('Uploading %s to %s/%s' % (file_path, container.name, cf_path.encode('utf-8')))
+    headers = {
+        'Cache-Control': 'max-age=%s' % get_max_age(cf_path),
+    }
+    print('Uploading %s to %s/%s headers: %s' % (file_path, container.name, cf_path, headers))
+
+    if dry_run:
+        return
+    driver = None
+    try:
+        driver = storage_driver(user, api_key)
+        driver.upload_object(file_path, container, cf_path, headers=headers)
+    except AttributeError as e:
+        print('AttributeError uploading file:', e)
+        # Probably a stupid mime type thing. Retry
+        driver.upload_object(file_path, container, cf_path, extra={'content_type': 'binary/octet-stream'}, headers=headers)
+    except Exception as e:
+        print('Error uploading file:', e)
+
+
+def delete_obj(obj, msg=None):
+    if msg:
+        print(msg)
+    else:
+        print('Deleting %s' % obj)
     if dry_run:
         return
     try:
-        container.upload_object(file_path, cf_path)
-    except AttributeError:
-        # Probably a stupid mime type thing. Retry
-        container.upload_object(file_path, cf_path, extra={'content_type': 'binary/octet-stream'})
+        driver = storage_driver(user, api_key)
+        driver.delete_object(obj)
     except Exception as e:
-        print('Error uploading file:', e)
+        print('Error deleting file:', e)
+
 
 if dry_run:
     print('(Dry run)')
@@ -66,16 +106,16 @@ except ContainerDoesNotExistError:
     container = provider.create_container(container_name)
 container_objects = provider.list_container_objects(container)
 
-if delete_all_files:
-    for obj in container_objects:
-        try:
-            print('Deleting %s' % obj)
-            obj.delete()
-        except Exception as e:
-            print('Error deleting file:', e)
-
 pprint(container)
 # pprint(container_objects)
+
+pool = Pool(20)
+
+if delete_all_files:
+    for obj in container_objects:
+        pool.spawn(delete_obj, obj)
+    pool.join()
+
 
 object_names = {x.name: x for x in container_objects}
 
@@ -87,24 +127,20 @@ for root, dirs, files in os.walk(jekyll_site):
             cf_path = '%s/%s' % (cf_root, file_name)
         else:
             cf_path = file_name
-        cf_path = cf_path.decode('utf-8')
         if object_names.get(cf_path):
             file_md5 = md5(file_path)
             if object_names[cf_path].hash != file_md5:
-                upload_file(container, file_path, cf_path)
+                pool.spawn(upload_file, container, file_path, cf_path)
             del object_names[cf_path]
         else:
-            upload_file(container, file_path, cf_path)
+            pool.spawn(upload_file, container, file_path, cf_path)
+
+pool.join()
 
 if delete_files:
     for name, obj in object_names.items():
-        try:
-            print('%s no longer exists locally. Deleting remote copy.' % name)
-            if dry_run:
-                continue
-            obj.delete()
-        except Exception as e:
-            print('Error deleting file:', e)
+        pool.spawn(delete_obj, obj, '%s no longer exists locally. Deleting remote copy.' % name)
+    pool.join()
 
 if provider_type == Provider.CLOUDFILES_US:
     print('Enabling CDN')
@@ -113,3 +149,5 @@ if provider_type == Provider.CLOUDFILES_US:
     container.enable_cdn(ex_ttl=ttl)
     cdn_url = container.get_cdn_url()
     print('All done! Your site is at %s' % cdn_url)
+else:
+    print('All done!')
