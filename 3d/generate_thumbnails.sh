7daca477ca4ce68aaa168bb47ce72cc36a136802
Geoff Greer <geoff@greer.fm>
2023-02-11 14:49:15 -0800

    Clean up thumbnail generation. Allow skipping already-generated thumbnails.

--- 3d/generate_thumbnails.sh
+++ 3d/generate_thumbnails.sh
@@ -6,6 +6,44 @@ image_size="800,600"
 
 file_paths=$(find . -name '*.stl')
 
+regenerate_all=true
+
+
+function render {
+  projection="$1"
+  camera="$2"
+  image_path="$3"
+  scad_path="$4"
+
+  if [ -f "$image_path" ] && [ -z "$regenerate_all" ]; then
+    echo "$image_path already exists. Skipping"
+    return;
+  fi
+  openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --projection="$projection" --camera="$camera" -o "$image_path" "$scad_path"
+}
+
+function render_all {
+  base_path="$1"
+  scad_path="$2"
+
+  # render multiple angles
+  # standard view
+  render perspective "0,0,0,55,0,25,0" "${base_path}-0.png" "$scad_path"
+  # rotate 90 degrees
+  render perspective "0,0,0,55,0,115,0" "${base_path}-1.png" "$scad_path"
+  # rotate 180 degrees
+  render perspective "0,0,0,55,0,205,0" "${base_path}-2.png" "$scad_path"
+  # top
+  render ortho "0,0,0,0,0,0,0" "${base_path}-top.png" "$scad_path"
+  # side
+  render ortho "0,0,0,90,0,0,0" "${base_path}-side.png" "$scad_path"
+  # front
+  render ortho "0,0,0,90,0,270,0" "${base_path}-front.png" "$scad_path"
+  # back
+  # openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --projection=ortho --camera=0,0,0,90,0,90,0 -o "${base_path}-back.png" "$file_path"
+}
+
+
 for file_path in $file_paths
 do
   dir_name=$(dirname "$file_path")
@@ -15,21 +53,8 @@ do
 
   tmp_scad="tmp_${base_name}.scad" # TODO: this can conflict if stl files in different dirs have the same name
   echo "import(\"${file_path}\");" > "${tmp_scad}"
-  # render multiple angles
-  # standard view
-  openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --camera=0,0,0,55,0,25,0 -o "${base_path}-0.png" "$tmp_scad" &
-  # rotate 90 degrees
-  openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --camera=0,0,0,55,0,115,0 -o "${base_path}-1.png" "$tmp_scad" &
-  # rotate 180 degrees
-  openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --camera=0,0,0,55,0,205,0 -o "${base_path}-2.png" "$tmp_scad" &
-  # top
-  openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --projection=ortho --camera=0,0,0,0,0,0,0 -o "${base_path}-top.png" "$tmp_scad" &
-  # side
-  openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --projection=ortho --camera=0,0,0,90,0,0,0 -o "${base_path}-side.png" "$tmp_scad" &
-  # front
-  openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --projection=ortho --camera=0,0,0,90,0,270,0 -o "${base_path}-front.png" "$tmp_scad"
-  # back
-  # openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --projection=ortho --camera=0,0,0,90,0,90,0 -o "${base_path}-back.png" "$tmp_scad"
+  render_all "$base_path" "$tmp_scad"
+
   rm "${tmp_scad}"
 done
 
@@ -44,19 +69,5 @@ do
   echo "Generating preview images for $file_path"
   base_path="${dir_name}/${base_name}"
 
-  # render multiple angles
-  # standard view
-  openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --camera=0,0,0,55,0,25,0 -o "${base_path}-0.png" "$file_path" &
-  # rotate 90 degrees
-  openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --camera=0,0,0,55,0,115,0 -o "${base_path}-1.png" "$file_path" &
-  # rotate 180 degrees
-  openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --camera=0,0,0,55,0,205,0 -o "${base_path}-2.png" "$file_path" &
-  # top
-  openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --projection=ortho --camera=0,0,0,0,0,0,0 -o "${base_path}-top.png" "$file_path" &
-  # side
-  openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --projection=ortho --camera=0,0,0,90,0,0,0 -o "${base_path}-side.png" "$file_path" &
-  # front
-  openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --projection=ortho --camera=0,0,0,90,0,270,0 -o "${base_path}-front.png" "$file_path"
-  # back
-  # openscad --render --imgsize="$image_size" --viewall --autocenter --view=axes,scales --projection=ortho --camera=0,0,0,90,0,90,0 -o "${base_path}-back.png" "$file_path"
+  render_all "$base_path" "$file_path"
 done
